3ee4f89d69bd1d12db191f6f77bdb252ad5d78df
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-27 01:11:11 +0200

    Create react app

---
+++ src/containers/WeatherForecast.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class WeatherForecast extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="weather-forecast">
+        <h1>Hello weather</h1>
+      </div>
+    );
+  }
+}
+
+export default WeatherForecast;
