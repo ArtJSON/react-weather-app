6becd2c98c2591f9e2117c5e1519aff63356364d
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-28 02:13:16 +0200

    Why not working

--- src/containers/WeatherForecast.js
+++ src/containers/WeatherForecast.js
@@ -44,6 +44,8 @@ class WeatherForecast extends Component {
         `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=${this.state.units}&include=days%2Ccurrent&key=${process.env.REACT_APP_WEATHER_API_KEY}&contentType=json`
       );
 
+      console.log(process.env.REACT_APP_WEATHER_API_KEY);
+
       this.setState({
         weather: weathRes.data.days,
         currentWeather: weathRes.data.currentConditions,
