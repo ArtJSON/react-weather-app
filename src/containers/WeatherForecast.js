c2bb4405db216f4ed3c2e624d6057147bd297ff6
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-28 03:06:39 +0200

    Add default state

--- src/containers/WeatherForecast.js
+++ src/containers/WeatherForecast.js
@@ -11,7 +11,7 @@ class WeatherForecast extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: "",
+      location: "Lodz,Poland",
       currentWeather: {},
       weather: [],
       units: "metric", //"us"
